74c16b81e047989ce96e7068dbeadd045e7454ad
Malgorzata Nguyen <mkn112@ic.ac.uk>
2017-11-26 21:23:03 +0000

    First version of GUI

---
+++ a.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'a.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(708, 531)
+        self.centralWidget = QtGui.QWidget(MainWindow)
+        self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.centralWidget)
+        self.verticalLayout.setMargin(11)
+        self.verticalLayout.setSpacing(6)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.widget = QtGui.QWidget(self.centralWidget)
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.widget)
+        self.horizontalLayout_2.setMargin(11)
+        self.horizontalLayout_2.setSpacing(6)
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.graphicsView = QtGui.QGraphicsView(self.widget)
+        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
+        self.horizontalLayout_2.addWidget(self.graphicsView)
+        self.graphicsView_2 = QtGui.QGraphicsView(self.widget)
+        self.graphicsView_2.setObjectName(_fromUtf8("graphicsView_2"))
+        self.horizontalLayout_2.addWidget(self.graphicsView_2)
+        self.verticalLayout.addWidget(self.widget)
+        self.frame = QtGui.QFrame(self.centralWidget)
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName(_fromUtf8("frame"))
+        self.gridLayout_3 = QtGui.QGridLayout(self.frame)
+        self.gridLayout_3.setMargin(11)
+        self.gridLayout_3.setSpacing(6)
+        self.gridLayout_3.setObjectName(_fromUtf8("gridLayout_3"))
+        self.pushButton = QtGui.QPushButton(self.frame)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout_3.addWidget(self.pushButton, 1, 0, 1, 1)
+        self.pushButton_3 = QtGui.QPushButton(self.frame)
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.gridLayout_3.addWidget(self.pushButton_3, 1, 1, 1, 1)
+        self.btn = QtGui.QPushButton(self.frame)
+        self.btn.setObjectName(_fromUtf8("btn"))
+        self.gridLayout_3.addWidget(self.btn, 0, 0, 1, 1)
+        self.pushButton_2 = QtGui.QPushButton(self.frame)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.gridLayout_3.addWidget(self.pushButton_2, 0, 1, 1, 1)
+        self.verticalLayout.addWidget(self.frame)
+        self.gridLayout = QtGui.QGridLayout()
+        self.gridLayout.setMargin(11)
+        self.gridLayout.setSpacing(6)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.label_2 = QtGui.QLabel(self.centralWidget)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
+        self.label_3 = QtGui.QLabel(self.centralWidget)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
+        self.spinBox_2 = QtGui.QSpinBox(self.centralWidget)
+        self.spinBox_2.setObjectName(_fromUtf8("spinBox_2"))
+        self.gridLayout.addWidget(self.spinBox_2, 1, 1, 1, 1)
+        self.spinBox = QtGui.QSpinBox(self.centralWidget)
+        self.spinBox.setObjectName(_fromUtf8("spinBox"))
+        self.gridLayout.addWidget(self.spinBox, 0, 1, 1, 1)
+        self.verticalLayout.addLayout(self.gridLayout)
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QtGui.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 708, 26))
+        self.menuBar.setObjectName(_fromUtf8("menuBar"))
+        MainWindow.setMenuBar(self.menuBar)
+        self.mainToolBar = QtGui.QToolBar(MainWindow)
+        self.mainToolBar.setObjectName(_fromUtf8("mainToolBar"))
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
+        self.statusBar = QtGui.QStatusBar(MainWindow)
+        self.statusBar.setObjectName(_fromUtf8("statusBar"))
+        MainWindow.setStatusBar(self.statusBar)
+        self.toolBar = QtGui.QToolBar(MainWindow)
+        self.toolBar.setObjectName(_fromUtf8("toolBar"))
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.toolBar_2 = QtGui.QToolBar(MainWindow)
+        self.toolBar_2.setObjectName(_fromUtf8("toolBar_2"))
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar_2)
+        self.toolBar_3 = QtGui.QToolBar(MainWindow)
+        self.toolBar_3.setObjectName(_fromUtf8("toolBar_3"))
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar_3)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.pushButton.setText(_translate("MainWindow", "Take Picture", None))
+        self.pushButton_3.setText(_translate("MainWindow", "Move Down", None))
+        self.btn.setText(_translate("MainWindow", "Hello", None))
+        self.pushButton_2.setText(_translate("MainWindow", "Move Up", None))
+        self.label_2.setText(_translate("MainWindow", "Set Interval", None))
+        self.label_3.setText(_translate("MainWindow", "Set Time", None))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar", None))
+        self.toolBar_2.setWindowTitle(_translate("MainWindow", "toolBar_2", None))
+        self.toolBar_3.setWindowTitle(_translate("MainWindow", "toolBar_3", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
