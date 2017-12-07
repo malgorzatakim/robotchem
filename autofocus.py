8f58a1cdb69b6790d54bfb02ffba4e85abb94e64
Malgorzata Nguyen <mkn112@ic.ac.uk>
2017-12-07 17:39:20 +0000

    made calcFocusing work with numpy

--- autofocus.py
+++ autofocus.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 from cameraoperator import CameraOperator
 from platform import Platform
+import time
 
 class Autofocus:
 
@@ -21,23 +22,25 @@ class Autofocus:
 
     def __calcFocusing__(self, image):
         im = image.convert("L") #opens pic and converts to grayscale
+        print time.time()
         width, height = im.size #gets image dimensions
         #pixels = list(im.getdata())
-        pixels = np.array(list(im.getdata()) #creates a list of all the pixel intensities
+        pixels = np.array(list(im.getdata()), dtype = "uint64") #creates a list of all the pixel intensities
         mean = np.mean(pixels) #calculates mean intensity of the picture
         if mean != 0: #so that we dont divide by 0
             #focusing = 1 / (height * width * mean) * sum([(x - mean) * (x - mean) for x in pixels])
-            #focusing = 1 / (height * width * mean) * ( sum([x * x for x in pixels]) - 2 * mean * sum(pixels) + mean * mean * len(pixels) )
+           # focusing = 1 / (height * width * mean) * ( sum([x * x for x in pixels]) - 2 * mean * sum(pixels) + mean * mean * len(pixels) )
             focusing = 1 / (height * width * mean) * (np.sum(np.power(pixels, 2)) 
                 -  2 * mean * np.sum(pixels) + mean * mean * pixels.shape[0])
         print focusing
+        print time.time()
         return focusing
 
     def autofocusSweep(self):
         sweepDone = False #put it in init?
 
         while sweepDone == False:
-            self.cameraOperato
+            self.cameraOperator
 
 
 
