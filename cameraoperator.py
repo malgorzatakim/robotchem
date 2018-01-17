e44f65c872f61f372ee0f41a9a9b974f373b31ae
malgorzata <maglorzatanguyen@dyn1228-102.wlan.ic.ac.uk>
2018-01-17 15:58:22 +0000

    attempt on fixing touchscreen issue

--- cameraoperator.py
+++ cameraoperator.py
@@ -21,8 +21,9 @@ class CameraOperator:
         #copy2(self.imageBasePath + src, self.currentSubfolder + filename) # for testing on a non-raspberry pi computer
         image = Image.open(self.currentSubfolder + filename)
         #opening the picture and cropping
-        image2 = (image.crop((390, 180, 1000, 790))).save(self.currentSubfolder + filename)
-        return Image.open(self.currentSubfolder + filename), filename
+        image2 = image.crop((390, 180, 1000, 790))
+        image2.save(self.currentSubfolder + filename)
+        return image2, filename
 
     def newSubfolder(self, masterFolder): # creates a new subfolder (called with timestamp) for a new dataset (either single pic or autofocus)
         name = time.time()
